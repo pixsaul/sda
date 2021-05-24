d2c3077eb15c47d2e44177341c23cf237a3fbc17
Saul Martin <saul@sauls-macbook-pro.local>
2021-05-24 12:07:17 +0000

    home setup

---
+++ site/snippets/header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width,initial-scale=1.0">
+
+  <title><?= $site->title() ?> | <?= $page->title() ?></title>
+
+  <?php
+  /*
+	Stylesheets can be included using the `css()` helper.
+	Kirby also provides the `js()` helper to include script file.
+	More Kirby helpers: https://getkirby.com/docs/reference/templates/helpers
+  */
+  ?>
+  <?= css([
+	'assets/css/style.css',
+	'@auto'
+  ]) ?>
+
+  <link rel="shortcut icon" type="image/x-icon" href="<?= url('favicon.ico') ?>">
+</head>
+<body>
+
+  <header class="header">
+
+  </header>
--- site/templates/home.php
+++ site/templates/home.php
@@ -1,4 +1,3 @@
 <?php snippet('header') ?>
-HOME
 <h1><?= $page->title() ?></h1>
 <p><?= $page->text() ?></p>
