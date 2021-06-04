3ca36ceeb54b94f80b4fe4c7709744a0ce4963e7
Saul Martin <saul@sauls-macbook-pro.local>
2021-06-04 09:37:18 +0000

    After Work Studio room config

--- assets/scenes/after-work-studio/config.js
+++ assets/scenes/after-work-studio/config.js
@@ -2,16 +2,86 @@ var path = '/sda/assets/scenes/after-work-studio/'
 roomConfig = {
   images: [
     {
-      path: path+'3d_After-Work_01.jpg',
+      path: path+'3d_After-Work_02.jpg',
       scale: 2.3,
       position: {
-        x: -1.7,
-        y: -0.3,
-        z: -1.7,
+        x: -2,
+        y: 1,
+        z: 0.01,
       },
       rotation: {
         x: 0,
         y: Math.PI/4,
+        z: 0,
+      }
+    },
+    {
+      path: path+'3d_After-Work_05.jpg',
+      scale: 2.3,
+      position: {
+        x: 0,
+        y: 0.2,
+        z: -2.7,
+      },
+      rotation: {
+        x: 0,
+        y: Math.PI/-1.1,
+        z: 0,
+      }
+    },
+    {
+      path: path+'3d_After-Work_06.jpg',
+      scale: 1.6,
+      position: {
+        x: 2.3,
+        y: -0.3,
+        z: -1.8,
+      },
+      rotation: {
+        x: 0,
+        y: Math.PI/3,
+        z: 0,
+      }
+    },
+    {
+      path: path+'3d_After-Work_01.jpg',
+      scale: 2.3,
+      position: {
+        x: 2.1,
+        y: -0.2,
+        z: 0,
+      },
+      rotation: {
+        x: 0,
+        y: Math.PI/1.5,
+        z: 0,
+      }
+    },
+    {
+      path: path+'3d_After-Work_04.jpg',
+      scale: 1.6,
+      position: {
+        x: 1.4,
+        y: -0.3,
+        z: 2,
+      },
+      rotation: {
+        x: 0,
+        y: Math.PI/4.2,
+        z: 0,
+      }
+    },
+    {
+      path: path+'3d_After-Work_03.jpg',
+      scale: 2.3,
+      position: {
+        x: -1.6,
+        y: -0.3,
+        z: 2.3,
+      },
+      rotation: {
+        x: 0,
+        y: Math.PI/1.1,
         z: 0,
       }
     }
