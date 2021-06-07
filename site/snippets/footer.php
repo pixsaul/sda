6d38d085e3f28bb296d2ca6b7d2eb394778a6d4e
Saul Martin <saul@sauls-macbook-pro.local>
2021-06-07 08:50:27 +0000

    participant placeholder pages added

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -2,9 +2,9 @@
 
   <footer class="footer">
     
-    <a href="https://www.instagram.com/swissdesignawards" target="_blank">Follow us</a>
+    <a target="_blank" href="https://www.instagram.com/swissdesignawards" target="_blank">Follow us</a>
     
-    <a href="#">newsletter</a>
+    <a target="_blank" href="https://admin.us19.list-manage.com/subscribe?u=e9dc04c185ae8e87a7152bca5&id=2d7f1db1b8">newsletter</a>
 
   </footer>
   
