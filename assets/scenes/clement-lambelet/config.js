9837aa1a12389895f7222244fda48268c11d91a5
zack dove <zdove@live.co.uk>
2021-06-07 21:54:33 +0100

    adding more to scenes

---
+++ assets/scenes/clement-lambelet/config.js
@@ -0,0 +1,105 @@
+var path = '/sda/assets/scenes/clement-lambelet/'
+roomConfig = {
+  images: [
+    {
+      path: path+'3d_Clement-Lambelet_01.jpg',
+      scale: 2.3,
+      position: {
+        x: -2,
+        y: 1,
+        z: 0.01,
+      },
+      rotation: {
+        x: 0,
+        y: Math.PI/4,
+        z: 0,
+      }
+    },
+    {
+      path: path+'3d_Clement-Lambelet_02.jpg',
+      scale: 2.3,
+      position: {
+        x: 0,
+        y: 0.2,
+        z: -2.7,
+      },
+      rotation: {
+        x: 0,
+        y: Math.PI/-1.1,
+        z: 0,
+      }
+    },
+    {
+      path: path+'3d_Clement-Lambelet_03.jpg',
+      scale: 1.6,
+      position: {
+        x: 2.3,
+        y: -0.3,
+        z: -1.8,
+      },
+      rotation: {
+        x: 0,
+        y: Math.PI/3,
+        z: 0,
+      }
+    },
+    {
+      path: path+'3d_Clement-Lambelet_04.jpg',
+      scale: 2.3,
+      position: {
+        x: 2.1,
+        y: -0.2,
+        z: 0,
+      },
+      rotation: {
+        x: 0,
+        y: Math.PI/1.5,
+        z: 0,
+      }
+    },
+    {
+      path: path+'3d_Clement-Lambelet_05.jpg',
+      scale: 1.6,
+      position: {
+        x: 1.4,
+        y: -0.3,
+        z: 2,
+      },
+      rotation: {
+        x: 0,
+        y: Math.PI/4.2,
+        z: 0,
+      }
+    },
+    {
+      path: path+'3d_Clement-Lambelet_06.jpg',
+      scale: 2.3,
+      position: {
+        x: -1.6,
+        y: -0.3,
+        z: 2.3,
+      },
+      rotation: {
+        x: 0,
+        y: Math.PI/1.1,
+        z: 0,
+      }
+    }
+  ],
+  blobs: [
+    {
+      path: path+'Clement-Lambelet.fbx',
+      scale: 0.25,
+      position: {
+        x: 0,
+        y: -2,
+        z: 0,
+      },
+      rotation: {
+        x: 0,
+        y: 0,
+        z: 0,
+      }
+    },
+  ]
+}
