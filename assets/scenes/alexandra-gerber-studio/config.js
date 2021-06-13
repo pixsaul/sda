c5be09286503ed66cea3c51816982015b473316c
zack dove <zdove@live.co.uk>
2021-06-13 14:45:14 +0100

    adding scene config for alexandra gerber

---
+++ assets/scenes/alexandra-gerber-studio/config.js
@@ -0,0 +1,122 @@
+var path = '/assets/scenes/alexandra-gerber-studio/'
+roomConfig = {
+  images: [
+    {
+      path: path+'3d_Alexandra-Gerber_01.jpg',
+      scale: 2.3,
+      position: {
+        x: 1.5,
+        y: -1.18,
+        z: -1.5,
+      },
+      rotation: {
+        x: 0,
+        y: Math.PI/4,
+        z: 0,
+      }
+    },
+    {
+      path: path+'3d_Alexandra-Gerber_02.jpg',
+	  scale: 2.3,
+ 	 position: {
+ 	   x: -1.5,
+ 	   y: -1.18,
+ 	   z: 1.5,
+ 	 },
+ 	 rotation: {
+ 	   x: 0,
+ 	   y: Math.PI/4,
+ 	   z: 0,
+ 	 }
+    },
+    {
+      path: path+'3d_Alexandra-Gerber_03.jpg',
+	  scale: 2.3,
+ 	 position: {
+ 	   x: -1.5,
+ 	   y: -1.18,
+ 	   z: -1.5,
+ 	 },
+ 	 rotation: {
+ 	   x: 0,
+ 	   y: -Math.PI/4,
+ 	   z: 0,
+ 	 }
+    },
+    {
+      path: path+'3d_Alexandra-Gerber_04.jpg',
+	  scale: 2.3,
+ 	position: {
+ 	  x: 1.5,
+ 	  y: -1.18,
+ 	  z: 1.5,
+ 	},
+ 	rotation: {
+ 	  x: 0,
+ 	  y: -Math.PI/4,
+ 	  z: 0,
+ 	}
+    },
+    {
+      path: path+'3d_Alexandra-Gerber_05.jpg',
+	  scale: 2.3,
+	position: {
+	  x: -1.5,
+	  y: 0.7,
+	  z: 1.5,
+	},
+	rotation: {
+	  x: 0,
+	  y: Math.PI/4,
+	  z: 0,
+	}
+    },
+    {
+      path: path+'3d_Alexandra-Gerber_06.jpg',
+	  scale: 2.3,
+ 	 position: {
+ 	   x: 1.5,
+ 	   y: 0.7,
+ 	   z: -1.5,
+ 	 },
+ 	 rotation: {
+ 	   x: 0,
+ 	   y: Math.PI/4,
+ 	   z: 0,
+ 	 }
+  },
+  
+  ],
+  blobs: [
+    {
+      path: path+'Alexandra-Gerber-studio.fbx',
+      scale: 0.25,
+      position: {
+        x: 0,
+        y: -2,
+        z: 0,
+      },
+      rotation: {
+        x: 0,
+        y: 0,
+        z: 0,
+      }
+    },
+],
+videos: [
+	{
+  	path: path+'3d_Alexandra-Gerber_07.mp4',
+  	scale: 1.5,
+  	position: {
+  	  x: 0,
+  	  y: 1,
+  	  z: 0,
+  	},
+  	rotation: {
+  	  x: 0,
+  	  y: 0,
+  	  z: 0,
+  	}
+    }
+]
+}
