efe0d7ea5c7c494f13ef47426676dcd540f7ad24
zack dove <zdove@live.co.uk>
2021-06-13 15:04:20 +0100

    scene config for fabien roy

---
+++ assets/scenes/fabien-roy-studio/config.js
@@ -0,0 +1,77 @@
+var path = '/assets/scenes/fabien-roy-studio/'
+roomConfig = {
+  images: [
+    {
+      path: path+'3d_Fabien-Roy_01.jpg',
+      scale: 2.5,
+      position: {
+        x: -1.3,
+        y: -0.1,
+        z: -1.3,
+      },
+      rotation: {
+        x: 0,
+        y: Math.PI/4,
+        z: 0,
+      }
+    },
+    {
+      path: path+'3d_Fabien-Roy_02.jpg',
+	  scale: 2.5,
+      position: {
+        x: -1.2,
+        y: -0.1,
+        z: -1.2,
+      },
+      rotation: {
+        x: 0,
+        y: Math.PI/4,
+        z: 0,
+      }
+    },
+    {
+      path: path+'3d_Fabien-Roy_03.jpg',
+	  scale: 2.5,
+ 	 position: {
+ 	   x: 1.3,
+ 	   y: -0.1,
+ 	   z: 1.1,
+ 	 },
+ 	 rotation: {
+ 	   x: 0,
+ 	   y: -Math.PI/4,
+ 	   z: 0,
+ 	 }
+    },
+	{
+      path: path+'3d_Fabien-Roy_04.jpg',
+	  scale: 2.5,
+ 	 position: {
+ 	   x: 1.1,
+ 	   y: -0.1,
+ 	   z: 1.3,
+ 	 },
+ 	 rotation: {
+ 	   x: 0,
+ 	   y: -Math.PI/4,
+ 	   z: 0,
+ 	 }
+    },
+  ],
+  blobs: [
+    {
+      path: path+'Fabien-Roy.fbx',
+      scale: 0.25,
+      position: {
+        x: 0,
+        y: -2,
+        z: 0,
+      },
+      rotation: {
+        x: 0,
+        y: 0,
+        z: 0,
+      }
+    },
+  ]
+}
