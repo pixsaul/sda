188b3a7f85183e48b0a74ae6fd6b81eb8000a20b
zack dove <zdove@live.co.uk>
2021-06-14 10:21:18 +0100

    AATB config

--- assets/scenes/aatb/config.js
+++ assets/scenes/aatb/config.js
@@ -1,94 +1,66 @@
-var path = '/assets/scenes/after-work-studio/'
+var path = '/assets/scenes/aatb/'
 roomConfig = {
   images: [
     {
-      path: path+'3d_After-Work_02.jpg',
+      path: path+'3d_AATB_01.jpg',
+      scale: 2.6,
+      position: {
+        x: -1.5,
+        y: 0.7,
+        z: -2,
+      },
+      rotation: {
+        x: 0,
+        y: 0,
+        z: 0,
+      }
+    },
+    {
+      path: path+'3d_AATB_02.jpg',
       scale: 2.3,
       position: {
-        x: -2,
-        y: 1,
-        z: 0.01,
+        x: -0.5,
+        y: -0.8,
+        z: -0.5,
       },
       rotation: {
         x: 0,
-        y: Math.PI/4,
+        y: 80*Math.PI/180,
         z: 0,
       }
     },
     {
-      path: path+'3d_After-Work_05.jpg',
-      scale: 2.3,
+      path: path+'3d_AATB_03.jpg',
+	  scale: 2.3,
       position: {
-        x: 0,
-        y: 0.2,
-        z: -2.7,
+        x: 0.5,
+        y: -0.8,
+        z: -0.5,
       },
       rotation: {
         x: 0,
-        y: Math.PI/-1.1,
+        y: -80*Math.PI/180,
         z: 0,
       }
     },
     {
-      path: path+'3d_After-Work_06.jpg',
-      scale: 1.6,
+      path: path+'3d_AATB_04.jpg',
+	  scale: 2.6,
       position: {
-        x: 2.3,
-        y: -0.3,
-        z: -1.8,
+        x: 1.5,
+        y: 0.7,
+        z: -2,
       },
       rotation: {
         x: 0,
-        y: Math.PI/3,
+        y: 0,
         z: 0,
       }
     },
-    {
-      path: path+'3d_After-Work_01.jpg',
-      scale: 2.3,
-      position: {
-        x: 2.1,
-        y: -0.2,
-        z: 0,
-      },
-      rotation: {
-        x: 0,
-        y: Math.PI/1.5,
-        z: 0,
-      }
-    },
-    {
-      path: path+'3d_After-Work_04.jpg',
-      scale: 1.6,
-      position: {
-        x: 1.4,
-        y: -0.3,
-        z: 2,
-      },
-      rotation: {
-        x: 0,
-        y: Math.PI/4.2,
-        z: 0,
-      }
-    },
-    {
-      path: path+'3d_After-Work_03.jpg',
-      scale: 2.3,
-      position: {
-        x: -1.6,
-        y: -0.3,
-        z: 2.3,
-      },
-      rotation: {
-        x: 0,
-        y: Math.PI/1.1,
-        z: 0,
-      }
-    }
   ],
   blobs: [
     {
-      path: path+'after-work-studio.fbx',
+      path: path+'AATB.fbx',
       scale: 0.25,
       position: {
         x: 0,
